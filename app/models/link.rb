b86ebc1db20c54849379620a83deb8ba5923feb3
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-11-04 12:22:31 +0000

    changeing database field

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,3 +11,5 @@ end
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
+
+# ENV['https://aqueous-sea-3289.herokuapp.com/'] ||
